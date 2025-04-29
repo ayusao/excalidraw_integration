83b8ec5f403b44c9734ba1a31caca25d2c5c8bcb
ayusao <077bct006.aayusha@pcampus.edu.np>
2025-04-29 21:43:10 +0545

    readme file

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXtH0qVyZgvoe5Y6R10t1UcLphGtw1wTk",
-  authDomain: "drawmate-c0069.firebaseapp.com",
-  projectId: "drawmate-c0069",
-  storageBucket: "drawmate-c0069.firebasestorage.app",
-  messagingSenderId: "178324777013",
-  appId: "1:178324777013:web:bd7e9316ee1662a302297c",
-  measurementId: "G-30XD1DL39M"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
